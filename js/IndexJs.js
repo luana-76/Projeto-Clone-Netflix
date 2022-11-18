56099a614fc0e230a43e9412cf52b932001e9d73
Luana Maria <luana_dark34@hotmail.com>
2022-11-18 16:02:17 -0300

    alterações

--- js/IndexJs.js
+++ js/IndexJs.js
@@ -3,149 +3,142 @@ class FunctionNetflix{
     constructor(){
 
         this.boxSearchEl = document.querySelector("#search");
-        this.lupaEl = document.querySelector('#pesqBut');
-        this.search = document.querySelector('#pesq');
-        this.boxsearch = document.querySelector('#searchBut');
-        this.acaoDeClickBotaoPesquisa();
-        this.boxSeta = document.querySelector('#listTwo');
-        this.seta = document.querySelector('#seta');
-        this.rotacaodeSeta();
-        this.scroll();
+        this.butBoxSearchEl = document.querySelector('#searchBut');
+        this.inputSearchEl = document.querySelector('#pesq');
+        this.searchButtonClickAction();
+
+        this.arrowBox = document.querySelector('#listTwo');
+        this.arrow = document.querySelector('#arrow');
+        this.arrowRotation();
+
+        this.section = [...document.querySelectorAll('main section')];
+        this.scrollEl = [...document.querySelectorAll('.scroll')];
+        this.direita = [...document.querySelectorAll('.arrowRight')];
+        this.esquerda = [...document.querySelectorAll(".arrownLeft")];
+        this.firstArrow = [...document.querySelectorAll('.right')];
+        this.setaTwo = [...document.querySelectorAll('.left')];
+        this.actionScroll();
         this.con=0
 
     }
 
-
     styleButSeach(background, border, transition, width, margin){
 
-        this.boxsearch.style.background = background;
-        this.boxsearch.style.border = border;
-        this.search.style.transition = transition;
-        this.search.style.width = width;
+        this.boxSearchEl.style.background = background;
+        this.boxSearchEl.style.border = border;
+        this.inputSearchEl.style.transition = transition;
+        this.inputSearchEl.style.width = width;
         this.boxSearchEl.style.margin = margin;
 
-    }
+    };
 
-    acaoDeClickBotaoPesquisa(){
+    searchButtonClickAction(){
 
-        this.lupaEl.addEventListener('click', event=>{
+        this.butBoxSearchEl.addEventListener('click', event=>{
 
-            this.search.focus();
+            this.inputSearchEl.focus();
             this.styleButSeach("rgb(20 20 20 / 68%)", "1px solid #fff", "width 0.5s", "200px", "0px 15px 0px 0px");
             
         });
 
-        this.search.addEventListener('blur', event=>{
+        this.inputSearchEl.addEventListener('blur', event=>{
 
-            if(this.search.value === ""){
+            if(this.inputSearchEl.value === ""){
 
                 this.styleButSeach("", "none", "width 0.5s", "0px", "0px 0px 0px 0px");
 
-            }
+            };
 
-        })
+        });
 
-    }
+    };
+    
+    arrowRotation(){
 
-    rotacaodeSeta(){
+        this.arrowBox.addEventListener('mouseover', event=>{
 
-        this.boxSeta.addEventListener('mouseover', event=>{
+            if(this.arrowBox.style.display == false){
 
-            if(this.boxSeta.style.display == false){
-
-                this.seta.style.transform = 'rotate(180deg)';
+                this.arrow.style.transform = 'rotate(180deg)';
     
             } 
 
         })
 
-        this.boxSeta.addEventListener('mouseleave', event=>{
+        this.arrowBox.addEventListener('mouseleave', event=>{
 
-            this.seta.style.transform = 'rotate(0deg)';
+            this.arrow.style.transform = 'rotate(0deg)';
 
         })
 
     }
 
-    rolacaoScroll(scroll, setaDireita, setaEsquerda, section, setaOne, setaTwo, cont){
+    scroll(scroll, rightArrow, leftArrow, section, firstArrow, setaTwo, cont){
 
-                //scroll.scrollIntoView({behavior: "smooth"})
+        //scroll.scrollIntoView({behavior: "smooth"})
     
-                    setaDireita.addEventListener('click', e=>{
-                        if(cont < 3)this.con++
-                        scroll.scrollTo(cont += 1210, 0);
-                        console.log(cont)
-                        this.scrollCreate(true, this.con)
+        rightArrow.addEventListener('click', e=>{
+
+            if(cont < 3)this.con++
+
+                scroll.scrollTo(cont += 1210, 0);
+                this.createScroll(true, this.con)
                         
-                        /*if(cont < 3630){
+                if(cont >= 1210){
+                          
+                    leftArrow.style.display = 'block'
+                    scroll.style.margin = "0 0 0 0"
+                    scroll.style.width = '98.6vw'
+
+                    section.addEventListener('mouseover', e=>{
             
-                            scroll.scrollTo(cont += 1210, 0);
-                            this.scrollCreate(true, this.con)
+                        firstArrow.style.display = "block";
+                        setaTwo.style.display = "block";
             
-                        }*/
-        
-                        if(cont >= 1210){
-        
-                            setaEsquerda.style.display = 'block'
-                            scroll.style.margin = "0 0 0 0"
-                            scroll.style.width = '98.6vw'
-                            section.addEventListener('mouseover', e=>{
-            
-                                setaOne.style.display = "block";
-                                setaTwo.style.display = "block";
-            
-                            })
-            
-                            section.addEventListener('mouseleave', e=>{
-            
-                                setaOne.style.display = "none";
-                                setaTwo.style.display = "none";
-            
-                            })
-                
-                        }
                     })
-        
-                setaEsquerda.addEventListener('click', e=>{
-                    /*this.con--*/
-                    if(cont > 3630) cont = 3630
-                    if(cont != 0){
-                        this.con--
-                        scroll.scrollTo(cont -= 1210, 0);
-                        this.scrollCreate(false, this.con)
-        
-                    }
-                    console.log(cont)
-                })
+            
+                    section.addEventListener('mouseleave', e=>{
+            
+                        firstArrow.style.display = "none";
+                        setaTwo.style.display = "none";
+            
+                    })
+                
+                }
 
-        }
-    scroll(){
+         })
+                
+        //Voltando scroll
+        leftArrow.addEventListener('click', e=>{
+                    
+            //Ele tá excluindo o da frente, então não vai ter como voltar mais de 3630
+            if(cont > 3630) cont = 3630
+            this.con--
+            scroll.scrollTo(cont -= 1210, 0);
+            this.createScroll(false, this.con)
+        })
 
-        this.section = [...document.querySelectorAll('.sectionsMoviesSeries')];
-        this.scrollEl = [...document.querySelectorAll('.scroll')];
-        this.direita = [...document.querySelectorAll('.setaDireita')];
-        this.esquerda = [...document.querySelectorAll('.setaEsquerda')];
-        this.setaOne = [...document.querySelectorAll('.direito')];
-        this.setaTwo = [...document.querySelectorAll('.esquerda')];
-        console.log(this.setaOne)
+    }
+
+    actionScroll(){
 
         if([...document.querySelectorAll('.scroll')][0]){
             let cont = 0
-            this.rolacaoScroll(this.scrollEl[0], this.direita[0],this.esquerda[0], this.section[0], this.setaOne[0],this.setaTwo[0], cont)
+            this.scroll(this.scrollEl[0], this.direita[0],this.esquerda[0], this.section[0], this.firstArrow[0],this.setaTwo[0], cont)
         }
+
         if([...document.querySelectorAll('.scroll')][1]){
             let cont = 0
-            this.rolacaoScroll(this.scrollEl[1], this.direita[1],this.esquerda[1], this.section[1], this.setaOne[1],this.setaTwo[1], cont)
+            this.scroll(this.scrollEl[1], this.direita[1],this.esquerda[1], this.section[1], this.firstArrow[1],this.setaTwo[1], cont)
         }
 
     
 
     }
 
-    scrollCreate(passou, cont){
+    createScroll(passou, cont){
 
-        //console.log([...document.querySelectorAll('.imagensSeries')])
-        let imgs = [...document.querySelectorAll('.imagensSeries')]
+        let imgs = [...document.querySelectorAll('.imgScroll')]
         let list = [imgs[0], imgs[1], imgs[2], imgs[3]]
         if(passou == true){
             if(cont => 2){
@@ -165,27 +158,14 @@ class FunctionNetflix{
         }
 
         if(passou == false){
-                console.log('certo')
-                
+                //console.log('certo')
+                //this.gambiarra(document.querySelector('.tela'))
                 //document.querySelector('.scroll').innerHTML = document.querySelector('.scroll')
 
         }
  
 
     }
-
-    /*gambiarra(element){
-
-        let div = document.createElement('div')
-        element.appendChild(div)
-
-        let imgOne = 
-        let imgOne = 
-        let imgOne = 
-        let imgOne = 
-
-
-    }*/
 
 }
 new FunctionNetflix()
